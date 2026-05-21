031575cb1cb74c179c97fe2887179daea2119faf
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-21 13:46:43 +0200

    Setting Up the Storage Module (async/await & try/catch)

---
+++ storage.js
@@ -0,0 +1,16 @@
+
+
+async function saveToLocal(data) {
+    try {
+        
+        const jsonData = JSON.stringify(data);
+
+       
+        localStorage.setItem("userPosts", jsonData);
+    } catch (err) {
+        
+        console.error(err);
+    }
+}
+
+export default saveToLocal;
