ece20e952d6e3210510d86cc1c5d219356d359f9
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-22 12:50:13 +0200

    updated api

--- api.js
+++ api.js
@@ -1,17 +1,13 @@
 function fetchPosts() {
     return fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+        .then(response => response.json())
+        .then(data => {
             return data;
-            
         })
-        .catch(function (error) {
-            console.log("Failed to fetch posts:", error);
+        .catch(error => {
+            console.error("Failed to fetch posts:", error);
             return [];
         });
 }
-fetchPosts()
+
 export { fetchPosts };
