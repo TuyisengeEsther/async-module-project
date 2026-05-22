9a3b909dc5808a6d0cb86becac0576a0a842f100
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-05-22 12:50:52 +0200

    updated storage

--- storage.js
+++ storage.js
@@ -1,10 +1,7 @@
 async function saveToLocal(data) {
     try {
         const jsonData = JSON.stringify(data);
-
         localStorage.setItem("userPosts", jsonData);
-
-        console.log(data);
     } catch (err) {
         console.error(err);
     }
